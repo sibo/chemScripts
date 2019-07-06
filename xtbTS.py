30e7ddbecd0464eeb1316c5595455c26f0ff190a
seebol <siboster@gmail.com>
2019-07-06 12:00:05 -0400

    Update xtbTS.py

--- xtbTS.py
+++ xtbTS.py
@@ -47,60 +47,70 @@ def distance(atom1, atom2):
 	return math.pow(math.pow(deltaX,2)+math.pow(deltaY,2)+math.pow(deltaZ,2),0.5)
 
 
-
 steps=100
 startScanDistance = getBondDistance(xyzFile,atomNo1,atomNo2)
-os.system("mkdir -p scan")
-os.system("cp " + xyzFile + " scan/")
-f=open("scan/xcontrol","w+")
-command = "$constrain \n  force constant = 1 \n  distance: " + str(atomNo1) + "," + str(atomNo2) + "," + str(startScanDistance) + "\n$scan\n  1: " + str(startScanDistance) + "," + str(finalScanDistance) + "," + str(steps) + " \n$end\n"
-f.write(command)
-f.close()
-os.chdir("./scan")
-os.system("xtb " + xyzFile + params + "--opt veryfine --input xcontrol | tee opt.out")
-f=open("xtbscan.log","r")
-f1=f.readlines()
-totalAtoms=int(f1[0])
-print "total atoms = " + str(totalAtoms)
-energies=[]
-structures=[]
-bondLengths=[]
-energy0 = float(f1[1].split()[-1])*627.509
-for i in range(0,steps):
-	energy = float(f1[i*(totalAtoms+2)+1].split()[-1])*627.509 - energy0
-	energies.append(energy)
-	print energy
-	coords = []
-	for line in f1[i*(totalAtoms+2)+2:(i+1)*(totalAtoms+2)]:
-		coords.append(line.split())
-	structures.append(coords)
-	bondLengths.append(distance(coords[atomNo1-1],coords[atomNo2-1]))
 
-f=open("scan.csv","w+")
-for i in range(0,len(energies)):
-	f.write(str(bondLengths[i]) + "," + str(energies[i]) + "\n")
-f.close()
+def PESscan(direction, xyzFile, atomNo1, atomNo2, finalScanDistance, startScanDistance, steps):
+	os.system("mkdir -p scan_" + direction)
+	os.system("cp " + xyzFile + " scan_" + direction + "/")
+	f=open("scan_" + direction + "/xcontrol","w+")
+	command = "$constrain \n  force constant = 1 \n  distance: " + str(atomNo1) + "," + str(atomNo2) + "," + str(startScanDistance) + "\n$scan\n  1: " + str(startScanDistance) + "," + str(finalScanDistance) + "," + str(steps) + " \n$end\n"
+	f.write(command)
+	f.close()
+	os.chdir("./scan_" + direction)
+	os.system("xtb " + os.path.basename(xyzFile) + params + "--opt veryfine --input xcontrol | tee opt.out")
+	f=open("xtbscan.log","r")
+	f1=f.readlines()
+	totalAtoms=int(f1[0])
+	print "total atoms = " + str(totalAtoms)
+	energies=[]
+	structures=[]
+	bondLengths=[]
+	energy0 = float(f1[1].split()[-1])*627.509
+	for i in range(0,steps):
+		energy = float(f1[i*(totalAtoms+2)+1].split()[-1])*627.509
+		energies.append(energy)
+		print energy
+		coords = []
+		for line in f1[i*(totalAtoms+2)+2:(i+1)*(totalAtoms+2)]:
+			coords.append(line.split())
+		structures.append(coords)
+		bondLengths.append(distance(coords[atomNo1-1],coords[atomNo2-1]))
 
-plt.scatter(bondLengths,energies)
-plt.xlabel(r"Bond Distance ($\AA$)")
-plt.ylabel("relative E (kcal/mol)")
-plt.savefig("scanPES.png")
-#plt.show()
+	f=open("scan.csv","w+")
+	for i in range(0,len(energies)):
+		f.write(str(bondLengths[i]) + "," + str(energies[i]) + "\n")
+	f.close()
 
-maxEnergy = energies[0]
-maxStruct = []
-for i in range(1,len(energies)):
-    if energies[i] > maxEnergy:
-        maxEnergy = energies[i]
-        maxStruct = structures[i]
+	plt.scatter(bondLengths,energies)
+	plt.xlabel(r"Bond Distance ($\AA$)")
+	plt.ylabel("relative E (kcal/mol)")
+	plt.savefig("scanPES.png")
+	
 
-f=open("TSguess.xyz","w+")
-f.write(str(totalAtoms) + "\n TS guess \n")
-for coord in maxStruct:
-    f.write(" ".join(coord) + "\n")
-f.close()
+	maxEnergy = energies[0]
+	maxStruct = []
+	for i in range(1,len(energies)):
+	    if energies[i] > maxEnergy:
+		maxEnergy = energies[i]
+		maxStruct = structures[i]
+	f=open("TSguess.xyz","w+")
+	f.write(str(totalAtoms) + "\n TS guess, E(xtb) = " + str(maxEnergy) + " \n")
+	for coord in maxStruct:
+	    f.write(" ".join(coord) + "\n")
+	f.close()
+	os.chdir("../")
+	return maxEnergy,maxStruct
+
+(maxEnergy1,maxStruct1)=PESscan("forward", xyzFile, atomNo1, atomNo2, finalScanDistance, startScanDistance,steps)
+(maxEnergy2,maxStruct2)=PESscan("backward","scan_forward/xtbopt.xyz", atomNo1, atomNo2, startScanDistance, finalScanDistance,steps)
+
+
 os.system("mkdir -p sp_ohess")
-os.system("cp TSguess.xyz sp_ohess/")
+if maxEnergy1 < maxEnergy2:
+	os.system("cp scan_forward/TSguess.xyz sp_ohess/")
+else:
+	os.system("cp scan_backward/TSguess.xyz sp_ohess/")
 os.chdir("./sp_ohess")
 f=open("xcontrol","w+")
 command = "$constrain \n  force constant = 1 \n  distance: " + str(atomNo1) + "," + str(atomNo2) + "," + str(getBondDistance("TSguess.xyz",atomNo1,atomNo2)) + "\n"
