c1e0ab8afe78472584b38f0046280795e63a607a
sibo <siboster@gmail.com>
2020-09-09 21:18:44 -0400

    Create jsmol.bat

---
+++ jsmol.bat
@@ -0,0 +1,6 @@
+REM batch file adapted from https://serverfault.com/questions/863030/launch-html-file-in-chrome-with-parameters
+REM killing all open Chrome windows...
+taskkill /F /IM chrome.exe /T
+REM starting JSMol from Chrome...
+@echo off
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" "file:///%CD%/jsmol.htm" --user-data-dir="C:/chromedev" --allow-file-access-from-files --new-window
